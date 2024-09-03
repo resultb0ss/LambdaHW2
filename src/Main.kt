18cf11200ad4d5130db715ca3a0fc181908dc6a1
Admin <reteamboss@gmail.com>
2024-09-03 21:21:22 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
 fun main() {
-    println("Hello World!")
+    var array: ArrayList<Int>  = arrayListOf(1,2,3,4,5,6,7)
+
+    var midval = { array: ArrayList<Int> -> array.sum() / array.size  }
+    println(midval(array))
 }
